e19e319d36c1070f96c24272a03de84076d7e953
tenklo <tenklo@pm.me>
2020-06-15 18:51:09 +0200

    initial commit

---
+++ blocks.h
@@ -0,0 +1,17 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"",	"lockerstatus",	2,	0},
+
+	{"",	"~/.local/src/dwm/dwmblocks/scripts/wifi",	5,	21},
+	{"",	"~/.local/src/dwm/dwmblocks/scripts/ssid",	5,	20},
+	{"🎶:",	"~/.local/src/dwm/dwmblocks/scripts/volume",	1,	19},
+	{":",   "xbacklight | sed 's/\\..*//'",					1,		11},
+	{":",	"~/.local/src/dwm/dwmblocks/scripts/rootspace",	30,	18},
+	{":",	"~/.local/src/dwm/dwmblocks/scripts/homespace",	30,	17},
+	{":",	"~/.local/src/dwm/dwmblocks/scripts/battery",	30,	16},
+	{"",	"date '+%d.%m.%Y (%a) %H:%M:%S'",	5,	0},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim = '|';
