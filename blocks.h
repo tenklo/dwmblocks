ad38a53753f65ba29bb8629838b9b3baef914f97
tenklo <tenklo@pm.me>
2020-07-21 22:16:36 +0200

    weather status

--- blocks.h
+++ blocks.h
@@ -1,6 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"",	"wetterstatus",	60,	0},
 	{"",	"orphans",	60,	0},
 	{" ",	"lockerstatus",	2,	69},
 	{"",	"~/.local/src/dwm/dwmblocks/scripts/wifi",	    5,	21},
