6702868fb4b03164fd3851581ea464a5e494b1f4
tenklo <tenklo@pm.me>
2021-04-18 18:32:52 +0200

    display new mail indicator

--- blocks.h
+++ blocks.h
@@ -2,6 +2,7 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"; ",	    "lockerstatus",	2,	69},
+	{"",	    "newmail",	60,	0},
 	{"",	    "orphans -s",	60,	0},
 	{" 🎶 ",    "volume",	1,	19},
 	{"",        "sbrightness",				1,	11},
